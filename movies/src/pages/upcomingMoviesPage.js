73f182e9789767eb6fcb263c95d8307e5e1551f9
Jamie Conlon <20085217@mail.wit.ie>
2023-10-24 20:45:49 +0100

    Change upcoming movies page to use caching.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -1,15 +1,29 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
+
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
 
 import MustWatchIcon from "../components/cardIcons/mustWatch";
 
 
 const UpcomingMoviesPage = (props) => {
-  const [movies, setMovies] = useState([]);
-  const favorites = movies.filter(m => m.favorite)
-  localStorage.setItem('favorites', JSON.stringify(favorites))
+  const {data, error, isLoading, isError }  = useQuery('upcoming', getUpcomingMovies)
 
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const upcoming = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
+  // const favorites = movies.filter(m => m.favorite)
+  // localStorage.setItem('favorites', JSON.stringify(favorites))
+  // const addToFavorites = (movieId) => true 
   // const addToFavorites = (movieId) => {
   //   const updatedMovies = movies.map((m) =>
   //     m.id === movieId ? { ...m, favorite: true } : m
@@ -18,16 +32,16 @@ const UpcomingMoviesPage = (props) => {
   // };
 
 
-  useEffect(() => {
-    getUpcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-  }, []);
+  // useEffect(() => {
+  //   getUpcomingMovies().then(movies => {
+  //     setMovies(movies);
+  //   });
+  // }, []);
 
   return (
     <PageTemplate
       title='Upcoming Movies'
-      movies={movies}
+      movies={upcoming}
       action={(movie) => {
         return <MustWatchIcon movie={movie} />
       }}
